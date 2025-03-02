0f196d4ea809d88102a04fb8a4d2cdfc54c15713
ashvinck <ashvinck10@gmail.com>
2025-03-02 10:31:47 +0530

    add data to mongo atlas

--- src/index.js
+++ src/index.js
@@ -28,7 +28,8 @@ const httpServer = createServer(app);
 // app.set('io', io); // using set method to mount the `io` instance on the app to avoid usage of `global`
 
 app.use(helmet());
-app.use(express.json({ limit: '16kb' }));
+// app.use(express.json({ limit: '16kb' }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 // global middlewares
 app.use(
