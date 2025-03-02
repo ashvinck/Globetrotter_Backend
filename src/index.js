03f49ff90337ed6a01893e93eacf06164cc0468b
ashvinck <ashvinck10@gmail.com>
2025-03-02 10:41:44 +0530

    added limit to request body

--- src/index.js
+++ src/index.js
@@ -28,8 +28,7 @@ const httpServer = createServer(app);
 // app.set('io', io); // using set method to mount the `io` instance on the app to avoid usage of `global`
 
 app.use(helmet());
-// app.use(express.json({ limit: '16kb' }));
-app.use(express.json());
+app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 // global middlewares
 app.use(
